c1b0408fbde3f8fe2e65bd30bac7f741f6df2424
elen.kocak@coino.com <elen.kocak@coino.com>
2023-06-23 14:43:28 +0300

    leetcode questions

---
+++ 234.PalindromeLinkedList/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Dizi elemanlarını girin (virgülle ayrılmış):");
+        string input = Console.ReadLine();
+
+        string[] numbersAsString = input.Split(',');
+        int[] numbers = Array.ConvertAll(numbersAsString, int.Parse);
+        IsPalindromicList(numbers);
+        Console.ReadLine();
+
+    }
+    public static bool IsPalindromicList(int[] numbers)
+    {
+        int reversed = 0; int original = 0;
+        for (int j = 0; j < numbers.Length; j++)
+        {
+            original = (original * 10) + numbers[j];
+        }
+        //for (int k = 0; k < numbers.Length; k++)
+        //{
+        //   reversed=(reversed * 10) + (original % 10);
+        //    original /= 10;
+        //}
+        int result = original;
+        while (result > 0)
+        {
+            reversed = (reversed * 10) + (result % 10);
+            result /= 10;
+        }
+    
+        Console.WriteLine($"Orijinal: {original}, Reverse: {reversed}");
+        return reversed==original;
+    }
+}
